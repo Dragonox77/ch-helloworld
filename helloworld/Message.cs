200d605c29c65da9337d6d608c5c8a8e2217acb3
BuildTools <unconfigured@null.spigotmc.org>
2021-09-21 07:56:44 +0200

    Ajoutez des fichiers projet.

---
+++ helloworld/Message.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    class Message
+    {
+        public string HelloMessage {
+            get {
+                DateTime now = DateTime.Now;
+                if (now.DayOfWeek != DayOfWeek.Sunday && now.DayOfWeek != DayOfWeek.Saturday)
+                {
+                    if (now.Hour >= this._hourMatin && now.Hour < this._hourStartAprem)
+                    {
+                        return "Bon matin " + Environment.UserName;
+                    }
+                    if (now.Hour >= this._hourStartAprem && now.Hour < this._hourSoir)
+                    {
+                        return "Bon après midi " + Environment.UserName;
+
+                    }
+                }
+                return "Bon WeekEnd " + Environment.UserName;
+            }
+        }
+        private int _hourMatin;
+        private int _hourStartAprem;
+        private int _hourSoir;
+
+
+        public Message(int hourMatin = 9, int HourStartAprem = 13, int hourSoir = 18)
+        {
+            this._hourMatin = hourMatin;
+            this._hourStartAprem = HourStartAprem;
+            this._hourSoir = hourSoir;
+        }
+    }
+}
---
+++ helloworld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if(message == "exit")
+                {
+                    break;
+                }
+                if(message == "")
+                {
+                    Console.WriteLine(new Message().HelloMessage);
+                }
+            }
+        }
+    }
+}
